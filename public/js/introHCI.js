214d50bdb294b706a365cc60816805ee6f19e582
Ruochen Yang <elmerdanny.yang@gmail.com>
2016-02-10 03:30:10 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,22 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	//console.log("User clicked on project " + idNumber);
+	var proj = "/project/"+idNumber;
+	$.get(proj, addProject);
+	console.log(proj);
+}
+
+function addProject(result){
+	console.log(result);
+	var id = "#project"+result.id;
+
+	var html = "<div>"+
+					"<img class='detailsImage' src="+result.image+">"+
+					"<h1>"+result.date+"</h1>"+
+					result.summary+
+				"</div>";
+	$(id+" .details").html(html);
 }
 
 /*
@@ -35,4 +50,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", randomColor);
+
+}
+
+function randomColor(result){
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
